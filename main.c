e45b100c40eb4ce660e245c25e717f76fa3ebe52
54621685 <edfgtre9898@gmail.com>
2018-05-18 01:39:53 +0800

    sa

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	/* 變數宣告 */
+    int score1, score2, score3, score4;
+    /* 多重指定敘述 */
+    score1 = score2 = score3 = score4 = 25;
+    /* 顯示變數值 */
+    printf("1: %d\n", score1);
+    printf("2: %d\n", score2);
+    printf("3: %d\n", score3);
+    printf("4: %d\n", score4);
+	return 0;
+}
